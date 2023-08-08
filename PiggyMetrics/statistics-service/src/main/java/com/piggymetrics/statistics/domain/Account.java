eac3a29686bab90b9b8ef5a993cb6d6eb549c574
parkseolhwa <cun2744@naver.com>
2023-08-08 17:36:51 +0900

    test

--- PiggyMetrics/statistics-service/src/main/java/com/piggymetrics/statistics/domain/Account.java
+++ PiggyMetrics/statistics-service/src/main/java/com/piggymetrics/statistics/domain/Account.java
@@ -1,6 +1,5 @@
 package com.piggymetrics.statistics.domain;
 
-import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.validation.Valid;
@@ -8,7 +7,6 @@ import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Document(collection = "accounts")
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class Account {
 
 	@Valid
