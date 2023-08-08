fa50ce03866108f9275a034f845b3556f0989a47
parkseolhwa <cun2744@naver.com>
2023-08-08 17:11:36 +0900

    test

--- PiggyMetrics/statistics-service/src/main/java/com/piggymetrics/statistics/domain/Account.java
+++ PiggyMetrics/statistics-service/src/main/java/com/piggymetrics/statistics/domain/Account.java
@@ -1,5 +1,6 @@
 package com.piggymetrics.statistics.domain;
 
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.validation.Valid;
