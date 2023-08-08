dd05fec330cee5530bd49ef32d6ff7c21f63781e
parkseolhwa <cun2744@naver.com>
2023-08-08 17:07:09 +0900

    d

--- PiggyMetrics/statistics-service/src/main/java/com/piggymetrics/statistics/StatisticsApplication.java
+++ PiggyMetrics/statistics-service/src/main/java/com/piggymetrics/statistics/StatisticsApplication.java
@@ -4,24 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
-//@EnableOAuth2Client
-//@EnableFeignClients
-// @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class StatisticsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StatisticsApplication.class, args);
 	}
 
-	// @Configuration
-	// static class CustomConversionsConfig {
-
-	// 	@Bean
-	// 	public CustomConversions customConversions() {
-	// 		return new CustomConversions(Arrays.asList(new DataPointIdReaderConverter(),
-	// 				new DataPointIdWriterConverter()));
-	// 	}
-	// }
 }
 
